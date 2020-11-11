32c73b8eadf5c3709e58f5853ff762d707fe399a
Akhil <akhilaand@gmail.com>
2020-11-11 22:53:44 +0530

    first commit

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:eco/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Home_Screen()
+
+    );
+  }
+}
