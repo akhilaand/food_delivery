ddb6750b98b478a70b1ff4089f1775570c83f206
Akhil <akhilaand@gmail.com>
2020-11-14 00:48:46 +0530

    implemented order details

--- lib/components/selected_food.dart
+++ lib/components/selected_food.dart
@@ -1,20 +1,53 @@
+import 'package:eco/data/food_cart_data.dart';
 import 'package:flutter/material.dart';
 class Selected_Food extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return Container(
+      height: 250,
+      child: ListView.builder(
+          itemCount: cart.length,
+          scrollDirection: Axis.horizontal,
 
-        itemBuilder: null);
+          itemBuilder:(BuildContext context,int index){
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+
+              child: Column(
+                children: <Widget>[
+                  CustomPaint(
+
+                    child: Container(
+                      height: 170,
+                      decoration: BoxDecoration(
+                       borderRadius: BorderRadius.only(topRight: Radius.circular(15),
+                       topLeft: Radius.circular(15)
+                       ),
+
+                        image: DecorationImage(
+                          fit: BoxFit.fill,
+                            image: AssetImage(cart[index].food_img)
+                        )
+                      ),
+
+                    ),
+                  )
+                ],
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
+              height: MediaQuery.of(context).size.height/2.8,
+              width: MediaQuery.of(context).size.width/2.0,
+
+            ),
+          );
+          }),
+    );
 
 
-//    Container(
-//      decoration: BoxDecoration(
-//          color: Colors.yellow,
-//        borderRadius: BorderRadius.all(Radius.circular(15))
-//      ),
-//      height: MediaQuery.of(context).size.height/2.8,
-//      width: MediaQuery.of(context).size.width/2.0,
-//
-//    );
+
   }
 }
--- lib/data/food_cart_data.dart
+++ lib/data/food_cart_data.dart
@@ -2,5 +2,17 @@ import 'package:eco/modal/food_cart.dart';
 
 final _food1=Food_Cart(
   food_cost: "\$20",
-  food_text: "Espresso"
+  food_text: "Espresso",
+    food_img:"assets/images/donut.jpg"
 );
+
+final _food2=Food_Cart(
+    food_cost: "\$10",
+    food_img: "assets/images/cappucino.jpeg",
+    food_text: "cappuciano"
+);
+
+final List<Food_Cart>cart=[
+  _food1,
+  _food2
+];
